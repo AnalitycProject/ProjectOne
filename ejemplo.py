8be21049c1ab4858373aec80deb7861127fa8b76
rc327-cell <rc3274678@gmail.com>
2025-09-18 01:22:58 +0000

    usuario3

--- ejemplo.py
+++ ejemplo.py
@@ -7,7 +7,7 @@ def saludar_usuario():
     nombre = input("¡Hola! ¿Cuál es tu nombre? ")
     print(f"¡Mucho gusto, {nombre}! Bienvenido a tu Codespace de Python en GitHub.")
 
-    print("\n hola soy el nuevo miembro pasaba a saludarte")
+    print("\n hola soy el nuevo miembro solo pasaba a saludarte")
 
 # Llamar a la función para que el programa se ejecute
 if __name__ == "__main__":
